b2442cd5422a020c93b9ac224b1fb32c639db82c
Yuriy Volkov <sir.volkov@outlook.com>
2023-11-22 22:02:49 +0200

    upd : globalstyle, formik & yup added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar.';
+// import { Axios } from 'axios';
+
+// Axios.defaults.baseURL =
+//   '<https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12/>';
+
+export class App extends Component {
+  state = {
+    text: '',
+  };
+
+  render() {
+    return (
+      <>
+        <Searchbar className="searchbar" />
+        <ul className="image-gallery" />
+        <button className="load-more" />
+        <div className="loader" />
+      </>
+    );
+  }
+}
